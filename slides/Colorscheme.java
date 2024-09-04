31a59667ed07b4c496e2d4952b407c2c634f2b61
Jim <jmb15@williams.edu>
2024-09-04 15:23:43 -0400

    poosh.bat

---
+++ slides/Colorscheme.java
@@ -0,0 +1,22 @@
+class Main {
+    public static void main(String[] arguments) {
+        int bar;
+        if (false) {
+            int bar = 0;
+        } else {
+            bar = 1;
+        }
+        System.out.println(bar);
+    }
+} 
+
+    
+    
+    class Colorscheme {
+        public static void main(String[] arguments) {
+            int i = 0;
+            char c = 'a';
+            double d = Double.NEGATIVE_INFINITY; // hello
+            boolean b = (2 + 2 == 4);
+        }
+    }
